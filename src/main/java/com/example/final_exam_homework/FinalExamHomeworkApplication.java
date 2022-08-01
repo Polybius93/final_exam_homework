d70df27e68d21f6a0e52fd575ea59f0f254c1a60
Polybius93 <adam.daniel.puskas@polybius-studios.com>
2022-08-01 11:36:46 +0200

    Starts project

---
+++ src/main/java/com/example/final_exam_homework/FinalExamHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_exam_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExamHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalExamHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/final_exam_homework/models/Item.java
@@ -0,0 +1,84 @@
+package com.example.final_exam_homework.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private User user;
+
+    private String description;
+
+    @JsonProperty(value = "photo_url")
+    private String photoUrl;
+
+    @JsonProperty(value = "starting_price")
+    private int startingPrice;
+
+    @JsonProperty(value = "purchase_price")
+    private int purchasePrice;
+
+    public Item() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public int getStartingPrice() {
+        return startingPrice;
+    }
+
+    public void setStartingPrice(int startingPrice) {
+        this.startingPrice = startingPrice;
+    }
+
+    public int getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(int purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+}
